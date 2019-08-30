0d23ce4f9e75b71ab3ff25d19c36568dc7d5e8d2
Sunghwan Bang <sunghwan2789@gmail.com>
2019-08-30 21:38:23 +0000

    기본 db로 mysql 사용

--- server/run.sh
+++ server/run.sh
@@ -3,5 +3,6 @@ sudo docker run -it --rm \
   -v `pwd`/credential.json:/credential.json \
   -v `pwd`/../test:/test_api \
   -e GOOGLE_APPLICATION_CREDENTIALS=/credential.json \
+  -v `pwd`/src/server:/server \
   server bash
 
--- server/src/server/server/settings.py
+++ server/src/server/server/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'server', # DB명
+        'USER': 'root', # 데이터베이스 계정
+        'PASSWORD': '', # 계정 비밀번호
+        'HOST': '', # 데이테베이스 주소(IP)
+        'PORT': '', # 데이터베이스 포트(보통은 3306)
     }
 }
 
