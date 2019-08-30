cebad8100d18f5944af785614009370b6bf3485f
Sunghwan Bang <sunghwan2789@gmail.com>
2019-08-30 21:20:55 +0000

    모델 추가

--- server/src/server/api/models.py
+++ server/src/server/api/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=150)
+    passwordQuestion = models.CharField(max_length=150)
+    passwordQuestionAnswer = models.CharField(max_length=150)
+    # phone = models.CharField(max_length=15)
+    # gender = models.CharField(max_length=10)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Video(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=150)
+    description = models.TextField()
+    pub_date = models.DateTimeField()
+    thumbnail = models.FilePathField()
+    filepath = models.FilePathField()
